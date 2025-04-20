73647c10a8845705e67797a864eb404794bc4514
CyZsh0x <rykxue.0fx7@gmail.com>
2025-04-20 13:42:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ app.get('/get', async (req, res) => {
 });
 
 // Serve frontend
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
